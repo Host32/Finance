8cf9dbe54f6f3a00a6a6928d44b33b1db50a3e29
Ivan <Ivan>
2016-02-24 00:33:37 -0300

    template structure

---
+++ client/finances/finances.js
@@ -0,0 +1,62 @@
+Template.finances.helpers({
+    receitas: function () {
+        return [
+            {
+                categoria: 'Salário',
+                dezembro: 1000,
+                janeiro: 1000,
+                fevereiro: 1000,
+                marco: 1000,
+                abril: 1000,
+                maio: 1000
+            }, {
+                categoria: 'Investimentos',
+                dezembro: 1000,
+                janeiro: 1000,
+                fevereiro: 1000,
+                marco: 1000,
+                abril: 1000,
+                maio: 1000
+            }, {
+                categoria: 'Outros',
+                dezembro: 1000,
+                janeiro: 1000,
+                fevereiro: 1000,
+                marco: 1000,
+                abril: 1000,
+                maio: 1000
+            }
+            
+        ]
+    },
+    despesasFixas: function (param) {
+        return [
+            {
+                categoria: 'Salário',
+                dezembro: 1000,
+                janeiro: 1000,
+                fevereiro: 1000,
+                marco: 1000,
+                abril: 1000,
+                maio: 1000
+            }, {
+                categoria: 'Investimentos',
+                dezembro: 1000,
+                janeiro: 1000,
+                fevereiro: 1000,
+                marco: 1000,
+                abril: 1000,
+                maio: 1000
+            }, {
+                categoria: 'Outros',
+                dezembro: 1000,
+                janeiro: 1000,
+                fevereiro: 1000,
+                marco: 1000,
+                abril: 1000,
+                maio: 1000
+            }
+            
+        ]
+    }
+});
